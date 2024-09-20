7d60d89b13da2956adf011bef04a59811dbd5346
Polyclinic Application <propcool56@gmail.com>
2024-09-20 22:17:42 +0400

    Second Commit lab1

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import numpy as np
 import sys
+import typing as tp
+import numpy.typing as ntp
 
-def read_properties(path):
-    '''Чтение параметров из файла'''
+def read_properties(path : str) -> dict[str, float]:
+    '''
+    Функция для чтения параметров из yaml файла
+
+    Принимаемые параметры:
+    path - строка с путём к файлу с параметрами
+
+    Возвращаемые значения:
+    properties - словарь с параметрами из файла
+    '''
     file = open(path, 'r')
     property_lines = file.readlines()
     file.close()
@@ -14,20 +24,42 @@ def read_properties(path):
         properties[key] = value
     return properties
 
-def write_results(path, arr):
-    '''Запись результатов'''
+def write_results(path : str, arr : tp.Iterable[float]) -> None:
+    '''
+    Функция для записи резулятатов вычислений в файл
+
+    Принимаемые параметры:
+    path - строка с путём к файлу для записи
+    arr - итератор по множеству результата
+
+    Возвращаемых значений нет.
+    '''
     file = open(path, 'w')
     for elem in arr:
         file.write(str(elem) + '\n')
     file.close()
-        
 
-def function_init(a, b, c):
-    '''Инициализация функции параметрами a, b, c'''
+def function_init(a : float, b : float, c : float) -> tp.Callable[[ntp.NDArray[np.float64]], ntp.NDArray[np.float64]]:
+    '''
+    Функция для инициализации функции из варианта параметрами a, b, c
+
+    Принимаемые параметры:
+    a, b, c - параметры функции из варианта
+
+    Возвращаемое значение:
+    Инициализированная параметрами функция, зависящая только от x.
+    '''
     return lambda x: a*np.exp(-b*x**2 + c*x)
 
-def config_properties():
-    '''Получение параметров из файла'''
+def config_properties() -> tuple[float, ...]:
+    '''
+    Функция для получения параметров из файла config.yaml
+
+    Принимаемых значений нет.
+
+    Возвращаемое значение:
+    Кортеж из параметров n0, h, nk, a, b, c
+    '''
     properties = read_properties("config.yaml")
     n0 = properties['n0']
     h = properties['h']
@@ -37,8 +69,15 @@ def config_properties():
     c = properties['c']
     return (n0, h, nk, a, b, c)
 
-def params_properties(params):
-    '''Получение параметров из массива'''
+def params_properties(params : list[str]) -> tuple[float, ...]:
+    '''
+    Функция для получения параметров из массива строк
+
+    Принимаемых значений нет.
+
+    Возвращаемое значение:
+    Кортеж из параметров n0, h, nk, a, b, c
+    '''
     n0 = float(params[0])
     h = float(params[1])
     nk = float(params[2])
@@ -47,20 +86,39 @@ def params_properties(params):
     c = float(params[5])
     return (n0, h, nk, a, b, c)
 
-def calculate_and_write(n0, h, nk, a, b, c):
-    '''Вычисление результата и записть в файл'''
+def calculate_and_write(n0 : float, h : float, nk : float, a : float, b : float, c : float) -> None:
+    '''
+    Функция для вычисления результа и записи в файл results.txt
+
+    Принимаемые значения:
+    n0, h, nk, a, b, c - параметры для вычисления значений функции
+
+    Возвращаемых значений нет.
+    '''
     n = int((nk - n0) / h)
     x = np.linspace(n0, nk, n)
     y = function_init(a, b, c)
     write_results("results.txt", y(x))
 
-def run_with_config():
-    '''Запуск программы с параметрами из файла конфигурации'''
+def run_with_config() -> None:
+    '''
+    Функция для запуска программы с параметрами из файла конфигурации
+
+    Принимаемых значений нет.
+
+    Возвращаемых значений нет.
+    '''
     properties = config_properties()
     calculate_and_write(*properties)
 
-def run_with_params(params):
-    '''Запуск программы с параметрами из консоли'''
+def run_with_params(params : list[str]) -> None:
+    '''
+    Функция для запуска программы с параметрами из консоли
+
+    Принимаемых значений нет.
+
+    Возвращаемых значений нет.
+    '''
     properties = params_properties(params)
     calculate_and_write(*properties)
 
